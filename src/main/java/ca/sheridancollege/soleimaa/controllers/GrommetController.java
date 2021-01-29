c023e57c58dfdd1c16d27365616c4f6cfc497c93
soleimaa <77124862+soleimaa@users.noreply.github.com>
2021-01-29 08:39:06 -0500

    Second Commit

--- src/main/java/ca/sheridancollege/soleimaa/controllers/GrommetController.java
+++ src/main/java/ca/sheridancollege/soleimaa/controllers/GrommetController.java
@@ -1,7 +1,24 @@
 package ca.sheridancollege.soleimaa.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GrommetController {
+
+    @GetMapping("/")
+    public String index(){
+
+
+        return "index";
+    }
+
+
+    @PostMapping ("/grommetFactory")
+    public String processGrommets(){
+
+
+        return "DisplayGrommets";
+    }
 }
